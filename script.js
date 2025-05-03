889e7bbf2738dae7bf2dcbd0e2dff20d09f82370
Layla Cibelle <laylacibelle004@gmail.com>
2025-05-03 16:28:35 -0300

    'Projeto final'

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function togglemode() {
     const img = document.querySelector("#profile img")
 
     if(html.classList.contains('light'))
-        img.setAttribute ('src', './assets/e884b184-9b87-4140-af9a-84c3f66e9e77-removebg-preview.png')
+        img.setAttribute ('src', './vs.code/assets/ChatGPTlight_files/e884b184-9b87-4140-af9a-84c3f66e9e77-removebg-preview.png')
     else {
-        img.setAttribute ('src', './assets/ChatGPT_Image_25_de_abr._de_2025__16_45_52-removebg-preview.png' )
+        img.setAttribute ('src', './vs.code/assets/ChatGPT_Image_25_de_abr._de_2025__16_45_52-removebg-preview.png' )
     }
 }
 
